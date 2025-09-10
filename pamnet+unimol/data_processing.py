621c78c44da775d368bfdfbd935501f9acb27b0c
Arun Sisarran <arunsisarran@gmail.com>
2025-09-09 21:58:49 -0400

    Added function to convert smiles to pytorch geometric data object

--- pamnet+unimol/data_processing.py
+++ pamnet+unimol/data_processing.py
@@ -1,5 +1,10 @@
 import logging
+from torch_geometric.data import Batch
 from unimol_tools import UniMolRepr
+from rdkit import Chem
+from rdkit import AllChem
+import torch
+from torch_geometric.data import Data
 
 logger = logging.getLogger(__name__)
 
@@ -17,17 +22,66 @@ class DataProcessing:
             remove_hs = False
         )
 
+    def smiles_to_pamnet(self, smiles):
+        mol = Chem.MolFromSmiles(smiles)
+
+        if mol is None:
+            return None
+        
+        mol.Chem.AddHs(mol)
+        AllChem.EmbedMolecule(mol, randomSeed=42)
+        AllChem.UFFOptimizeMolecule(mol)
+        
+        atomic_nums = [atom.GetAtomicNum() for atom in mol.GetAtoms()]
+        
+        conformer = mol.GetConformer()
+        positions = []
+        for i in range(mol.GetNumAtoms()):
+            pos = conformer.GetAtomPosition(i)
+            positions.append([pos.x, pos.y, pos.z])
+        
+        edge_indices = []
+        for bond in mol.GetBonds():
+            i = bond.GetBeginAtomIdx()
+            j = bond.GetEndAtomIdx()
+            edge_indices.extend([[i, j], [j, i]])
+
+        x = torch.tensor(atomic_nums, dtype=torch.long)
+        pos = torch.tensor(positions, dtype=torch.float32)
+
+        if edge_indices:
+            edge_index = torch.tensor(edge_indices, dtype=torch.long).t().contiguous()
+        else:
+            edge_index = torch.empty((2, 0), dtype=torch.long)
+        
+        return Data(x=x, edge_index=edge_index, pos=pos)
+
     def data_process_pamnet(self, smiles_list):
         if self.pamnet_model is None:
             logger.warning("PAMNet model is None")
             return None
-        
-        # TODO: pass in the data in the format pamnet expects
+
+        data_list = []
+        for smiles in smiles_list:
+            data = self.smiles_to_pamnet_data(smiles)
+            if data is not None:
+                data_list.append(data)
+
+        if not data_list:
+            logger.warning("No valid molecules for PAMNet")
+            return None
+
+        batch = Batch.from_data_list(data_list)
+
+        with torch.no_grad():
+            pamnet_output = self.pamnet_model(batch)
+
+        return pamnet_output
 
     def data_process_unimol(self, smiles_list):
         unimol_output = self.unimol_model.get_repr(
             smiles_list,
-            return_automatic_reprs = True
+            return_atomic_reprs = True
         )
 
         return unimol_output
